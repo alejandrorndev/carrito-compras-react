a9010ba60147d6acf2bb96cafc95c0eea5f2ebc7
Alejandro restrepo <alejandrorn.dev@gmail.com>
2024-09-26 18:45:32 -0500

    fix:Se organizan los nombres de los productos

--- src/products.js
+++ src/products.js
@@ -9,7 +9,7 @@ import imageProduct8 from './assets/images/8.png'
 export const products = [
     {
         id: 1,
-        name: 'Hertfoid Upholstered Chair',
+        name: 'Silla tapizada Hertfoid',
         price: 101,
         image: imageProduct1,
         description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
@@ -17,7 +17,7 @@ export const products = [
     },
     {
         id: 2,
-        name: 'Abingdon Upholstered Chair Swivel',
+        name: 'Silla Tapizada',
         price: 151,
         image: imageProduct2,
         description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
@@ -25,7 +25,7 @@ export const products = [
     },
     {
         id: 3,
-        name: 'Jeses Minimore Modern Style Etta',
+        name: 'Silla Moderna Etta',
         price: 181,
         image: imageProduct3,
         description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
@@ -33,7 +33,7 @@ export const products = [
     },
     {
         id: 4,
-        name: 'JJeses Minimore Modern Style',
+        name: 'Silla Moderna Jjess',
         price: 201,
         image: imageProduct4,
         description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
@@ -41,7 +41,7 @@ export const products = [
     },
     {
         id: 5,
-        name: 'Bolanle Upholstered Armchair',
+        name: 'Sillón tapizado Bolanle',
         price: 251,
         image: imageProduct5,
         description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
@@ -49,7 +49,7 @@ export const products = [
     },
     {
         id: 6,
-        name: 'Jaqueze Upholstered Armchair',
+        name: 'Sillón tapizado Jaqueze',
         price: 111,
         image: imageProduct6,
         description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
@@ -57,7 +57,7 @@ export const products = [
     },
     {
         id: 7,
-        name: 'Leston Wide Upholstered Fabric',
+        name: 'Leston Ancho Tapizado Tela',
         price: 121,
         image: imageProduct7,
         description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
@@ -65,7 +65,7 @@ export const products = [
     },
     {
         id: 8,
-        name: 'Stephanny 27.5" Wide Tufted',
+        name: 'Stephanny 27.5" Ancho Acolchado',
         price: 220,
         image: imageProduct8,
         description: 'Mollit anim consectetur adipisicing aute pariatur ad mollit ad. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
